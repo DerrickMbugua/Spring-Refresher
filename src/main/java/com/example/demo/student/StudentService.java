085b42a7c25f22f5de5be550ea367d5af314e406
“derrick” <derrickmbugua50@gmail.com>
2023-01-17 15:04:04 +0300

    feat: save student using post mapping

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,11 +10,17 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
 
+    @Autowired
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     public List<Student> getStudents(){
        return studentRepository.findAll();
+    }
+
+
+    public void addStudent(Student student) {
+        studentRepository.save(student);
     }
 }
